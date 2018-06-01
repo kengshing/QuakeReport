581c637c0cab0d74d085518ba665bc7b91a12a80
Aw <kengshing.aw@orcacreation.net>
2018-06-01 21:08:24 +0800

    180601-2107 Desktop Assigned Spinner onItemSelectedListener. Added test url to test no quake condition

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -21,7 +21,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.content.Loader;
 import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager;
 import android.app.LoaderManager.LoaderCallbacks;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -40,7 +39,12 @@ import java.util.List;
 public class EarthquakeActivity extends AppCompatActivity implements LoaderCallbacks<List<Earthquake>> {
 
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
-
+    Toolbar mainToolbar;
+    Spinner mainSpinner;
+    String usgsRequestUrl;
+    TextView listLoadingMessageView;
+    ProgressBar listProgressBar;
+    TextView listEmptyView;
     /**
      * URL for earthquake data from the USGS dataset
      */
@@ -49,54 +53,66 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
 
     private EarthquakeAdaptor mAdapter;
 
-    Toolbar mainToolbar;
-    Spinner mainSpinner;
-    String usgsRequestUrl;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.earthquake_activity);
 
-       mainToolbar = findViewById(R.id.toolbar_view);
-       mainSpinner = findViewById(R.id.spinner_view);
+        listLoadingMessageView = findViewById(R.id.list_loading_message_view);
+        listProgressBar = findViewById(R.id.list_progress_bar);
+        listEmptyView = findViewById(R.id.list_empty_view);
+
+        mainToolbar = findViewById(R.id.toolbar_view);
+        mainSpinner = findViewById(R.id.spinner_view);
 
 
-       if (mainToolbar != null){
+        if (mainToolbar != null) {
 
-           setSupportActionBar(mainToolbar);
-           mainToolbar.setTitle(R.string.app_name);
-       } else {
-           Log.e(LOG_TAG, "mainToolbar is null");
-       }
-
-      ArrayAdapter<String> spinnerAdaptor = new ArrayAdapter<String>(EarthquakeActivity.this,R.layout.spinner_text_layoout,getResources().getStringArray(R.array.quake_type));
-     spinnerAdaptor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-      mainSpinner.setAdapter(spinnerAdaptor);
-
-      mainSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-          @Override
-          public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-          }
-
-          @Override
-          public void onNothingSelected(AdapterView<?> parent) {
-
-          }
-      });
-
-        usgsRequestUrl = getString(R.string.usgs_request_url_since2000);
-       refreshScreen();
-
+            setSupportActionBar(mainToolbar);
+            mainToolbar.setTitle(R.string.app_name);
+        } else {
+            Log.e(LOG_TAG, "mainToolbar is null");
         }
+
+        ArrayAdapter<String> spinnerAdaptor = new ArrayAdapter<String>(EarthquakeActivity.this, R.layout.spinner_text_layoout, getResources().getStringArray(R.array.quake_type));
+        spinnerAdaptor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mainSpinner.setAdapter(spinnerAdaptor);
+
+        mainSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                switch (position) {
+                    case 0:
+                        usgsRequestUrl = getString(R.string.usgs_request_url_recent);
+                        getLoaderManager().destroyLoader(0);
+                        refreshScreen();
+                        break;
+                    case 1:
+                        usgsRequestUrl = getString(R.string.usgs_request_url_since2000);
+                        getLoaderManager().destroyLoader(0);
+                        refreshScreen();
+                        break;
+                    case 2:
+                        usgsRequestUrl=getString(R.string.usgs_request_url_test);
+                        getLoaderManager().destroyLoader(0);
+                        refreshScreen();
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                usgsRequestUrl = getString(R.string.usgs_request_url_recent);
+                refreshScreen();
+            }
+        });
+    }
 
     @Override
     public android.content.Loader<List<Earthquake>> onCreateLoader(int i, Bundle bundle) {
-        //usgsRequestUrl = getString(R.string.usgs_request_url_since2000);
-        return new EarthquakeLoader(this, usgsRequestUrl);
+          return new EarthquakeLoader(this, usgsRequestUrl);
     }
 
     @Override
@@ -107,9 +123,8 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
     @Override
     public void onLoadFinished(android.content.Loader<List<Earthquake>> loader, List<Earthquake> data) {
 
-        TextView listEmptyView = findViewById(R.id.list_empty_view);
-        listEmptyView.setText(R.string.no_earthquake_message);
-        ProgressBar listProgressBar = findViewById(R.id.list_progress_bar);
+       listEmptyView.setText(null);
+        listLoadingMessageView.setVisibility(View.GONE);
         listProgressBar.setVisibility(View.GONE);
 
         //Clear the adapter of previous earthquake data
@@ -119,13 +134,15 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
         //dataset. This will trigger the ListView update
         if (data != null && !data.isEmpty()) {
             mAdapter.addAll(data);
-              }
+        }else {
+            listEmptyView.setText(R.string.no_earthquake_message);
+        }
     }
 
-    public void refreshScreen (){
+    public void refreshScreen() {
 
         // Find a reference to the {@link ListView} in the layout
-        ListView earthquakeListView = (ListView) findViewById(R.id.list);
+        ListView earthquakeListView = findViewById(R.id.list);
         earthquakeListView.setEmptyView(findViewById(R.id.list_empty_view));
 
         // Create a new {@link ArrayAdapter} of earthquakes
@@ -149,12 +166,12 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
         NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
         boolean isConnected = activeNetwork != null &&
                 activeNetwork.isConnectedOrConnecting();
-        if (isConnected){
+        if (isConnected) {
             getLoaderManager().initLoader(0, null, this);
-        }else {
-            TextView listEmptyView = findViewById(R.id.list_empty_view);
-            listEmptyView.setText(R.string.no_internet_message);
-            ProgressBar listProgressBar = findViewById(R.id.list_progress_bar);
+        } else {
+            //TextView listEmptyView = findViewById(R.id.list_empty_view);
+            //listEmptyView.setText(R.string.no_internet_message);
+            listLoadingMessageView.setText(R.string.no_internet_message);
             listProgressBar.setVisibility(View.GONE);
         }
 
